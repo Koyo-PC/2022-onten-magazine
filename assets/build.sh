33536492cae189d4ecd72e99c407cf5d8bf0b08e
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-25 17:23:56 +0900

    mathjax機能削除

--- assets/build.sh
+++ assets/build.sh
@@ -1,2 +1,2 @@
 mkdir ./out
-sudo pandoc --mathjax -s *.md -t html5 -c ./assets/github.css -c ./assets/wk.css --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./assets/toc.xsl" --pdf-engine=wkhtmltopdf -N -o ./out/output.pdf
+sudo pandoc -s *.md -t html5 -c ./assets/github.css -c ./assets/wk.css --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./assets/toc.xsl" --pdf-engine=wkhtmltopdf -N -o ./out/output.pdf
