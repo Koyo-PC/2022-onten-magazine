a980e1c635d12ea817532b67a34e5b4cfe81dff4
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-25 20:51:20 +0900

    権限関係調整

--- assets/build.sh
+++ assets/build.sh
@@ -1,4 +1,4 @@
 mkdir ./out
 sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s *.md -t html5 -c ./assets/github.css -c ./assets/wk.css --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./assets/toc.xsl" --pdf-engine=wkhtmltopdf -N -o ./out/output.pdf
 sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s *.md -t html5 -c ./assets/github.css -c ./assets/wk.css -N -o ./out/output.html
-wkhtmltopdf ./out/output.html toc --toc-header-text 目次 --xsl-style-sheet ./assets/toc.xsl ./out/output.pdf
+sudo wkhtmltopdf ./out/output.html toc --toc-header-text 目次 --xsl-style-sheet ./assets/toc.xsl --enable-local-file-access ./out/output.pdf
