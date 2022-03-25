5d43a4a1a7718faf3e5ba70a7603c08cf6ac07be
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-25 21:54:59 +0900

    build-pdfにpandocを利用

--- assets/build-pdf.sh
+++ assets/build-pdf.sh
@@ -1,1 +1,4 @@
-sudo wkhtmltopdf ./out/output.html toc --toc-header-text 目次 --xsl-style-sheet ./assets/toc.xsl --enable-local-file-access ./out/output.pdf
+cp ./assets/toc.xsl ./out/toc.xsl
+cd ./out/
+sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s ../*.md -t html5 -c ./github.css -c ./wk.css --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o ./output.pdf
+cd ../
