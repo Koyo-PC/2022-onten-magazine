8fd72a206a9f1c85e99ea355fcf3b0a45d64e4aa
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-26 15:07:55 +0900

    mathjaxの利用

--- assets/build-pdf.sh
+++ assets/build-pdf.sh
@@ -1,5 +1,6 @@
 cp ./assets/github.css ./out/github.css
 cp ./assets/wk.css ./out/wk.css
+cp ./assets/dynoload.js ./out/dynoload.js
 cp ./assets/toc.xsl ./out/toc.xsl
 mkdir ./out/Assets
 assets=($(find ./ -name "Assets" -type d))
@@ -11,6 +12,6 @@ mdpath=($(find ../ -name "*.md"))
 for mdfile in ${mdpath[@]}; do
   htfile=$(basename $(echo $mdfile | sed -e "s|\.md|.pdf|"))
   echo $mdfile
-  sudo pandoc --webtex='https://latex.codecogs.com/svg.latex?' -s -i $mdfile -t html5 -c ./github.css -c ./wk.css --self-contained --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o $htfile
+  sudo pandoc --mathjax=dynoload.js -s -i $mdfile -t html5 -c ./github.css -c ./wk.css --self-contained --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o $htfile
 done
 cd ../
