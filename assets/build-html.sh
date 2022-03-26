a0b962579791e8efcffe4e9aff65390c85cf2216
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-26 12:19:07 +0900

    echo $mdfile を追加

--- assets/build-html.sh
+++ assets/build-html.sh
@@ -4,6 +4,7 @@ cd ./out/
 mdpath=($(find ../ -name "*.md"))
 for mdfile in ${mdpath[@]}; do
   htfile=$(echo $mdfile | sed -e "s|\.md|.html|")
+  echo $mdfile
   sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s -i $mdfile -t html5 -c ./github.css -c ./wk.css -N -o $htfile
 done
 cd ../
--- assets/build-pdf.sh
+++ assets/build-pdf.sh
@@ -3,6 +3,7 @@ cd ./out/
 mdpath=($(find ../ -name "*.md"))
 for mdfile in ${mdpath[@]}; do
   htfile=$(echo $mdfile | sed -e "s|\.md|.html|")
+  echo $mdfile
   sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s -i $mdfile -t html5 -c ./github.css -c ./wk.css --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o $htfile
 done
 cd ../
