abcedf17cc751355e2f55cabb028d496be00593f
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-26 11:09:57 +0900

    mdファイル位置のワイルドカードミス

--- assets/build-pdf.sh
+++ assets/build-pdf.sh
@@ -1,4 +1,4 @@
 cp ./assets/toc.xsl ./out/toc.xsl
 cd ./out/
-sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s ../*.md -t html5 -c ./github.css -c ./wk.css --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o ./output.pdf
+sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s ../**/.md -t html5 -c ./github.css -c ./wk.css --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o ./output.pdf
 cd ../
