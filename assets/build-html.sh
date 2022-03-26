f826033d5f59d902ca199c460357b5968dbb83ec
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-26 11:21:27 +0900

    Pandocはワイルドカードを使えないらしい

--- assets/build-html.sh
+++ assets/build-html.sh
@@ -1,5 +1,9 @@
 cp ./assets/github.css ./out/github.css
 cp ./assets/wk.css ./out/wk.css
 cd ./out/
-sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s ../**/*.md -t html5 -c ./github.css -c ./wk.css -N -o ./output.html
+mdpath=($(find ../ -name "*.md"))
+for mdfile in ${mdpath[@]}; do
+  htfile=$(echo $mdfile | sed -e "s|\.md|.html|")
+  sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s -i $mdfile -t html5 -c ./github.css -c ./wk.css -N -o $htfile
+done
 cd ../
--- assets/build-pdf.sh
+++ assets/build-pdf.sh
@@ -1,4 +1,8 @@
 cp ./assets/toc.xsl ./out/toc.xsl
 cd ./out/
-sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s ../**/*.md -t html5 -c ./github.css -c ./wk.css --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o ./output.pdf
+mdpath=($(find ../ -name "*.md"))
+for mdfile in ${mdpath[@]}; do
+  htfile=$(echo $mdfile | sed -e "s|\.md|.html|")
+  sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s -i $mdfile -t html5 -c ./github.css -c ./wk.css --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o $htfile
+done
 cd ../
