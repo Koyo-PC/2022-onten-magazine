792ef903dbdf49487767775d41b806576ea101a6
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-26 11:11:25 +0900

    ワイルドカードの指定ミス

--- assets/build-html.sh
+++ assets/build-html.sh
@@ -1,5 +1,5 @@
 cp ./assets/github.css ./out/github.css
 cp ./assets/wk.css ./out/wk.css
 cd ./out/
-sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s ../*.md -t html5 -c ./github.css -c ./wk.css -N -o ./output.html
+sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s ../**/*.md -t html5 -c ./github.css -c ./wk.css -N -o ./output.html
 cd ../
