feb1cc670877ec24959591e480b6e115f25822ad
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-26 12:49:15 +0900

    bacename以外の方法を利用

--- assets/build-html.sh
+++ assets/build-html.sh
@@ -3,7 +3,7 @@ cp ./assets/wk.css ./out/wk.css
 cd ./out/
 mdpath=($(find ../ -name "*.md"))
 for mdfile in ${mdpath[@]}; do
-  htfile=$(echo $mdfile | basename | sed -e "s|\.md|.html|")
+  htfile=$(echo ${$mdfile##*/} | sed -e "s|\.md|.html|")
   echo $mdfile
   sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s -i $mdfile -t html5 -c ./github.css -c ./wk.css -N -o $htfile
 done
