70e6d61538618cae6dc4f13388dd8b80e3cf951d
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-26 12:23:17 +0900

    ../をout/に変更

--- assets/build-pdf.sh
+++ assets/build-pdf.sh
@@ -2,7 +2,7 @@ cp ./assets/toc.xsl ./out/toc.xsl
 cd ./out/
 mdpath=($(find ../ -name "*.md"))
 for mdfile in ${mdpath[@]}; do
-  htfile=$(echo $mdfile | sed -e "s|\.md|.html|")
+  htfile=$(echo $mdfile | sed -e "s|\.md|.html|" | sed -e "s|\..|out|")
   echo $mdfile
   sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s -i $mdfile -t html5 -c ./github.css -c ./wk.css --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o $htfile
 done
