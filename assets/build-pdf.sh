679be7c2cab7365a1a5519cbd227278c321f9a37
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-03-26 13:01:34 +0900

    pandocに画像埋め込み/webtexリンクstring化

--- assets/build-pdf.sh
+++ assets/build-pdf.sh
@@ -4,6 +4,6 @@ mdpath=($(find ../ -name "*.md"))
 for mdfile in ${mdpath[@]}; do
   htfile=$(basename $(echo $mdfile | sed -e "s|\.md|.pdf|"))
   echo $mdfile
-  sudo pandoc --webtex=https://latex.codecogs.com/svg.latex? -s -i $mdfile -t html5 -c ./github.css -c ./wk.css --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o $htfile
+  sudo pandoc --webtex='https://latex.codecogs.com/svg.latex?' -s -i $mdfile -t html5 -c ./github.css -c ./wk.css --serlf-contained --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o $htfile
 done
 cd ../
