908e567edbdacb4bbc25e1c084e9b112c70c8036
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-09-17 02:17:17 +0900

    add page marge

--- assets/build-pdf.sh
+++ assets/build-pdf.sh
@@ -1,29 +1,10 @@
-cd out
-
-### Minted
-
-# sudo apt install -y python3-pip
-# pip install pygments
-# pip install pandocfilters
-# wget https://raw.githubusercontent.com/nick-ulle/pandoc-minted/master/pandoc-minted.py
-# pandoc -s --filter ./pandoc-minted.py ../**/**/*.md -o output.tex -f markdown -t latex --pdf-engine lualatex --pdf-engine-opt=--shell-escape --pdf-engine-opt="-aux-directory=temp" -V documentclass=bxjsarticle -V classoption=pandoc -H ../assets/header.tex -o output.pdf
-
-pandoc -s ../**/**/*.md -o output.tex -f markdown -t latex --pdf-engine lualatex -V documentclass=bxjsarticle -V classoption=pandoc -H ../assets/header.tex -o output.pdf
-
-#cp ./assets/github.css ./out/github.css
-#cp ./assets/wk.css ./out/wk.css
-#cp ./assets/dynoload.js ./out/dynoload.js
-#cp ./assets/toc.xsl ./out/toc.xsl
-#mkdir ./out/Assets
-#assets=($(find ./ -name "Assets" -type d))
-#for assetfolder in ${assets[@]}; do
-#  cp -r $assetfolder ./out/
-#done
-#cd ./out/
-#mdpath=($(find ../ -name "*.md"))
-#for mdfile in ${mdpath[@]}; do
-#  htfile=$(basename $(echo $mdfile | sed -e "s|\.md|.pdf|"))
-#  echo $mdfile
-#  sudo pandoc --webtex='https://latex.codecogs.com/svg.latex?' -s -i $mdfile -t html5 -c ./github.css -c ./wk.css --self-contained --pdf-engine-opt="toc" --pdf-engine-opt="--toc-header-text" --pdf-engine-opt="目次" --pdf-engine-opt="--xsl-style-sheet" --pdf-engine-opt="./toc.xsl" --pdf-engine=wkhtmltopdf -N -o $htfile
-#done
-#cd ../
+cd `dirname $0`
+cd ../
+mkdir out
+mkdir tmp
+for f in `cat files.txt` do
+cat $f | md-to-pdf --md-file-encoding utf-8 --css https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css --body-class markdown-body > tmp/out.pdf
+pdfunite ./out/out.pdf ./tmp/out.pdf ./out/out_marged.pdf
+rm ./out/out.pdf
+mv ./out/out_marged.pdf ./out/out.pdf
+done
