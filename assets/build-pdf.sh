9c646426ba122ac2d56b58588c0bf6c647bc513e
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-09-17 02:49:35 +0900

    use basedir

--- assets/build-pdf.sh
+++ assets/build-pdf.sh
@@ -5,11 +5,7 @@ cp ./assets/dummy.pdf ./out/out.pdf
 for f in `cat files.txt`
 do
 mkdir tmp
-cp $f ./tmp/markdown.md
-cp `dirname $f`/Assets ./tmp/Assets
-cd tmp
-cat markdown.md | md-to-pdf --md-file-encoding utf-8 --css https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css --body-class markdown-body > out.pdf
-cd ../
+cat $f | md-to-pdf --md-file-encoding utf-8 --basedir `dirname $f` --css https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css --body-class markdown-body > tmp/out.pdf
 pdfunite ./out/out.pdf ./tmp/out.pdf ./out/out_marged.pdf
 rm ./out/out.pdf
 mv ./out/out_marged.pdf ./out/out.pdf
