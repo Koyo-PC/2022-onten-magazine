b35ee4ba0846386319c2d200e3efb25f7efed443
ZOI_dayo <zoizoi.dayo@gmail.com>
2022-09-17 02:25:25 +0900

    command error

--- assets/build-pdf.sh
+++ assets/build-pdf.sh
@@ -2,7 +2,8 @@ cd `dirname $0`
 cd ../
 mkdir out
 mkdir tmp
-for f in `cat files.txt` do
+for f in `cat files.txt`
+do
 cat $f | md-to-pdf --md-file-encoding utf-8 --css https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css --body-class markdown-body > tmp/out.pdf
 pdfunite ./out/out.pdf ./tmp/out.pdf ./out/out_marged.pdf
 rm ./out/out.pdf
